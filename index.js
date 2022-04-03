019d79a1d87faf821ca773fd362d3439234310a8
technerd21 <93470709+technerd21@users.noreply.github.com>
2022-04-03 23:41:56 +0500

    quizcv

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#show-cv-btn").click(function() {
+        $("#cv-container").show();
+    })
+
+
+    $(document).ready(function() {
+        $("#close-cv-btn").click(function() {
+            $("#cv-container").hide();
+        })
+})
+})
